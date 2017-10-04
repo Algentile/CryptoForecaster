99ecfb469069bfc57f02d58d34f0fae24eb266b3
algentile <gogentileorgohome@gmail.com>
2017-10-03 21:33:26 -0400

    added some placeholders for data in model

--- models/recurrent_model.py
+++ models/recurrent_model.py
@@ -2,7 +2,14 @@ import tflearn
 import numpy as np
 import tensorflow as tf 
 
-class RecurrentModel:
+class CryptoLSTM:
 
-	def __init__(self):
-		
+	def __init__(self, step_num, batch_size, data):
+		self.step_num = step_num
+		self.batch_size = batch_size
+		self.num_features = num_features
+		self.input = tf.placeholder(tf.float32, [batch_size, num_steps])
+		self.target = tf.placeholder(tf.float32, [batch_size, num_steps])
+
+	def construct_neural_network():
+
