badfef4ffee8ca89dafc92871142df1ee8845fcf
algentile <gogentileorgohome@gmail.com>
2017-11-05 10:56:09 -0500

    added a config parser

--- parsers/data_parser.py
+++ parsers/data_parser.py
@@ -3,46 +3,48 @@ import numpy as np
 from sys import argv
 import tensorflow as tf
 from tflearn.data_utils import load_csv
+import configparser as ConfigParser
 
 
-def check_number_features(csv_file, column_number):
+def check_feature_num(csv_file, column_number):
     with open(csv_file, newline='') as f:
         reader = csv.reader(f)
         row1 = next(reader)
+
     if((len(row1) - 1) < column_number or len(row1) == 0):
         raise Exception(
             'Column number out of range for output feature file of column length' + len(row1))
 
 
 def parse_csv(csv_file, column_number):
-    check_number_features(csv_file, column_number)
-    features, labels = load_csv(csv_file, target_column=column_number, columns_to_ignore=None, has_header=True)
-    feature_tensor = np.array(features).reshape(len(features), len(features[0])).astype(np.float)
+    check_feature_num(csv_file, column_number)
+
+    features, labels = load_csv(
+        csv_file, target_column=column_number, columns_to_ignore=None, has_header=True)
+
+    feature_tensor = np.array(features).reshape(
+        len(features), len(features[0])).astype(np.float)
+
     label_tensor = np.array(labels).reshape(len(labels), - 1).astype(np.float)
 
     return feature_tensor, label_tensor
 
-"""
-Grabs user input from the command line and returns a dictionary of both the 
-training features and labels as well as the testing features and labels.
-Input:
-	1. Training file --> 70% -- 80% base dataset
-	2. testing file  --> 30% -- 20% base dataset
-Output:
-	file_dict --> dictionary of two tuples containing training and testing tensors.
-"""
+
+def parse_options(settings_dict, parser, section):
+    options = parser.options(section)
+    for option in options:
+        if(parser.get(section, option) == ''):
+            raise Exception(
+                'Error when processing config file with option ' + str(option))
+        else:
+            settings_dict[option] = parser.get(section, option)
 
 
-def get_user_input(col):
-
-    train_file = argv[1]
-    test_file = argv[2]
-
-    train_x, train_y = parse_csv(
-        '../datasets/cryptocurrencypricehistory/' + train_file, column_number=col)
-    test_x, test_y = parse_csv(
-        '../datasets/cryptocurrencypricehistory/' + test_file, column_number=col)
-
-    file_dict = {'train': (train_x, train_y), 'test': (test_x, test_y)}
-
-    return file_dict
+def get_config_map(config_file):
+    settings_dict = {}
+    parser = ConfigParser.ConfigParser()
+    parser.read(config_file)
+    sections = parser.sections()
+    for section in sections:
+        parse_options(settings_dict, parser, section)
+    return settings_dict
---
+++ tests/data_parser_test.py
@@ -0,0 +1,34 @@
+import unittest
+from parsers.data_parser import *
+
+
+class DataParserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.settings_dict = get_config_map('test_files/config.ini')
+
+    def tearDown(self):
+        self.settings_dict = None
+
+    def test_parse_config(self):
+        train_file = self.settings_dict['train_file']
+        self.assertTrue(
+            str(train_file) == '/Users/algentile/Documents/CryptoForecaster/eth_train_file.csv')
+
+    def test_broken_config(self):
+        with self.assertRaises(Exception) as context:
+            self.broken_dict = get_config_map('test_files/broken_config.ini')
+
+        self.assertTrue('Error when processing config file with option column_number' in str(
+            context.exception))
+    
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(DataParserTest('test_parse_config'))
+    suite.addTest(DataParserTest('test_broken_config'))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
